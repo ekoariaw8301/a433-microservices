9ef5b890b2d4e22fb40b9e7d9f17b7ab35510bd1
ekoariaw8301 <51822765+ekoariaw8301@users.noreply.github.com>
2023-12-12 01:53:04 +0700

    update

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-#build docker image from Dockerfile in current dir with name karsajobs tag latest
-#docker build -t ekoariaw8301/karsajobs-ui:latest .
 
-#build docker image from github package
+# build Docker image dari berkas Dockerfile
 docker build -t ghcr.io/ekoariaw8301/karsajobs-ui:latest .
 
-# login ke docker hub
-#echo $PASSWORD_DOCKER_HUB | docker login -u ekoariaw8301 --password-stdin
-
-# login ke github package
+# login ke GitHub Package
 echo $CR_PAT | docker login ghcr.io -u ekoariaw8301 --password-stdin
 
-#docker push ekoariaw8301/karsajobs-ui:latest
+# push image ke GitHub Package
 docker push ghcr.io/ekoariaw8301/karsajobs-ui:latest

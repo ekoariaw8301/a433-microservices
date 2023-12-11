ab09701d1c3f2bee7a71081e2d9e05393bab3620
ekoariaw8301 <51822765+ekoariaw8301@users.noreply.github.com>
2023-12-12 02:05:04 +0700

    update all

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,36 +1,10 @@
 #!/bin/sh
-#build docker image from Dockerfile in current dir with name karsajobs tag latest
-#docker build -t ekoariaw8301/karsajobs:latest .
 
-#build docker image from dockerfile
+# build Docker image dari berkas Dockerfile
 docker build -t ghcr.io/ekoariaw8301/karsajobs:latest .
 
-# login ke docker hub
-#echo $PASSWORD_DOCKER_HUB | docker login -u ekoariaw8301 --password-stdin
-
-# login ke github package
+# ogin ke GitHub Package
 echo $CR_PAT | docker login ghcr.io -u ekoariaw8301 --password-stdin
 
-# puch docker ke docker hub
-# docker push ekoariaw8301/karsajobs:latest
-
-#push docker ke github package
+# push image ke GitHub Package
 docker push ghcr.io/ekoariaw8301/karsajobs:latest
-
-#add new line to tidy output
-#echo '\n\n'
-
-#create new image tag
-#docker tag karsajobs:latest ghcr.io/ariecc101/karsajobs:latest
-#add new line to tidy output
-#echo '\n\n'
-
-#login to github packages
-#echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u ariecc101 --password-stdin
-#add new line to tidy output
-#echo '\n\n'
-
-#push image to github packages
-#docker push ghcr.io/ariecc101/karsajobs:latest
-#add new line to tidy output
-#echo '\n\n'

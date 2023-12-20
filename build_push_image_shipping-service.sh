01566e59d1206865946eb31b36d581a3ed0f92b3
ekoariaw8301 <51822765+ekoariaw8301@users.noreply.github.com>
2023-12-20 11:35:12 +0700

    initial commit

---
+++ build_push_image_shipping-service.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#docker build -t ekoariaw8301/shipping-service:latest .
+docker build -t ghcr.io/ekoariaw8301/shipping-service:latest .
+
+#echo $PASSWORD_DOCKER_HUB | docker login -u ekoariaw8301 --password-stdin
+echo $CR_PAT | docker login ghcr.io -u ekoariaw8301 --password-stdin
+
+#docker push ekoariaw8301/shipping-service:latest
+docker push ghcr.io/ekoariaw8301/shipping-service:latest
